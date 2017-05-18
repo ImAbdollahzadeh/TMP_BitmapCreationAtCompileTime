a2397c94a033138d7e76bc90aaf7fef6636950e8
Iman Abdollahzadeh <iman.ab2013@gmail.com>
2017-05-18 14:03:58 +0200

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -1,6 +1,7 @@
+#include <iostream>
 #define construct typedef
-struct _Null {};
-template<typename... _ListOfPixel> struct ConcatPixels;
+struct null {};
+
 namespace Bitwise {
 	template<int First, int Second> struct And {
 		static constexpr int result = First & Second;
@@ -16,177 +17,213 @@ namespace Bitwise {
 	};
 }
 enum Color {
-	RED   = 0x00FF0000,
-	GREEN = 0x0000FF00,
-	BLUE  = 0x000000FF,
+	RED   = 0x00ff0000,
+	GREEN = 0x0000ff00,
+	BLUE  = 0x000000ff,
 	WHITE = Bitwise::Or <Bitwise::Or <RED, GREEN>::result, BLUE>::result,
 	BLACK = Bitwise::And<Bitwise::And<RED, GREEN>::result, BLUE>::result
 };
-template<typename TrueType, typename FalseType> struct SameAs {
-	static constexpr bool result = false;
+template<int r, int g, int b> struct RGB {
+	static constexpr int Red   = r;
+	static constexpr int Green = g;
+	static constexpr int Blue  = b;
 };
-template<typename TrueType> struct SameAs<TrueType, TrueType> {
-	static constexpr bool result = true;
+template<int _color> struct RGB_From_Color {
+	static constexpr int Red   = Bitwise::RightBitShift<Bitwise::And <_color, 0x00ff0000>::result, 0x10>::result;
+	static constexpr int Green = Bitwise::RightBitShift<Bitwise::And <_color, 0x0000ff00>::result, 0x08>::result;
+	static constexpr int Blue  = Bitwise::RightBitShift<Bitwise::And <_color, 0x000000ff>::result, 0x00>::result;
 };
-template<bool cond, typename TrueType, typename FalseType> struct _if {
-	typedef TrueType type;
+template<int x, int y, typename _RGB> struct pixel {
+	static constexpr int X = x;
+	static constexpr int Y = y;
+	static constexpr int Red   = _RGB::Red;
+	static constexpr int Green = _RGB::Green;
+	static constexpr int Blue  = _RGB::Blue;
 };
-template<typename TrueType, typename FalseType> struct _if<false, TrueType, FalseType> {
-	typedef FalseType type;
+struct _int_0x14_byte {
+	const int X_coor;
+	const int Y_coor;
+	const int RED;
+	const int GREEN;
+	const int BLUE;
+	constexpr _int_0x14_byte(int F, int S, int r, int g, int b) :
+		X_coor(F), Y_coor(S), RED(r), GREEN(g), BLUE(b)
+	{}
+	constexpr _int_0x14_byte() :
+		X_coor(0), Y_coor(0), RED(0), GREEN(0), BLUE(0)
+	{}
 };
-template<Color first, Color... colors> struct BlendColors {
-	static constexpr Color BlendedResult =
-		Bitwise::Or<first, BlendColors<colors...>::BlendedResult>::result;
-};
-template<Color last> struct BlendColors<last> {
-	static constexpr Color BlendedResult = last;
-};
-template<Color first> struct Subtract_Red {
-	static constexpr Color SubtractedResult = Bitwise::Or<first, Color::RED>::result;
-};
-template<Color first> struct Subtract_Green {
-	static constexpr Color SubtractedResult = Bitwise::Or<first, Color::GREEN>::result;
-};
-template<Color first> struct Subtract_Blue {
-	static constexpr Color SubtractedResult = Bitwise::Or<first, Color::BLUE>::result;
-};
-template<Color first> struct Subtract_White {
-	static constexpr Color SubtractedResult = Bitwise::Or<first, Color::WHITE>::result;
-};
-template<Color first> struct Subtract_Black {
-	static constexpr Color SubtractedResult = Bitwise::Or<first, Color::BLACK>::result;
-};
-template<Color color, template<Color>typename Mode> struct Subtract {
-	static constexpr Color SubtractedResult = 
-		_if<SameAs<Mode<color>, Subtract_Red<color>>::result, Subtract_Red<color>, 
-			_if<SameAs<Mode<color>, Subtract_Green<color>>::result, Subtract_Green<color>, 
-				_if<SameAs<Mode<color>, Subtract_Blue<color>>::result, Subtract_Blue<color>, 
-					_if<SameAs<Mode<color>, Subtract_White<color>>::result, Subtract_White<color>, 
-						_if<SameAs<Mode<color>, Subtract_Black<color>>::result, Subtract_Black<color>, _Null
-						>::type
-					>::type
-				>::type
-			>::type
-		>::type::SubtractedResult;
-};
-template<int x, int y, int z> struct RGB {
-	static constexpr int Red   = Bitwise::Or<Bitwise::And<Color::RED,   0x00>::result, x>::result;
-	static constexpr int Green = Bitwise::Or<Bitwise::And<Color::GREEN, 0x00>::result, y>::result;
-	static constexpr int Blue  = Bitwise::Or<Bitwise::And<Color::BLUE,  0x00>::result, z>::result;
-};
-template<Color color> struct RGBFromColor {
-	static constexpr int Red   = Bitwise::Or<Color::RED,   color>::result;
-	static constexpr int Green = Bitwise::Or<Color::GREEN, color>::result;
-	static constexpr int Blue  = Bitwise::Or<Color::BLUE,  color>::result;
-};
-template<int x, int y, typename _RGB> struct Pixel {
-	static constexpr int X           = x;
-	static constexpr int Y           = y;
-	static constexpr int Red_Value   = _RGB::Red;
-	static constexpr int Green_Value = _RGB::Green;
-	static constexpr int Blue_Value  = _RGB::Blue;
-};
-template<typename FirstPixel, typename... px> struct ListOfPixels {
-	typedef FirstPixel head;
-	typedef ListOfPixels<px...> rest;
-};
+typedef _int_0x14_byte __int20;
+constexpr int ModifiedNumberOfArgumentsInOf__int20 = (sizeof(__int20) - 0x01)/sizeof(int);
 
-template<typename List> struct SizeOfListOfPixels {
-	typedef typename List::FirstPixel head;
-	typedef typename List::rest rest;
-	static constexpr int __size = 1 + SizeOfListOfPixels<rest>::__size;
+template<typename pix> struct PixelEncoder {
+	static constexpr __int20 encode = __int20(
+		pix::X, pix::Y, pix::Red, pix::Green, pix::Blue
+	);
 };
-template<> struct SizeOfListOfPixels<_Null> {
-	static constexpr int __size = 0;
+template<size_t sz, typename...px> struct __Bitmap {
+	static constexpr int BitmapPixelSize = sz;
+	static constexpr int BitmapLightPixelsListSize = sizeof...(px);
+	static constexpr __int20 LightBitmap[sizeof...(px)] = { PixelEncoder<px>::encode... };
 };
+template<size_t Size> struct Combine {
+	static int FinalArray[Size*Size * sizeof(__int20)];
+	static int* perform(const __int20* lightPixels, size_t sizeOfPixelList) {
+		size_t count = 0;
+		while (count != sizeOfPixelList) {
+			for (size_t i = 0; i != Size*Size; ++i) {
+				__int20 tmp = lightPixels[count];
+				if (i != (tmp.X_coor)*Size + (tmp.Y_coor)) {
+					FinalArray[i * 4] = i;
+					FinalArray[(i * 4) + 1] = 0;
+					FinalArray[(i * 4) + 2] = 0;
+					FinalArray[(i * 4) + 3] = 0;
+				}
+				else {
+					FinalArray[i * 4] = (tmp.X_coor)*Size + (tmp.Y_coor);
+					FinalArray[(i * 4) + 1] = tmp.RED;
+					FinalArray[(i * 4) + 2] = tmp.GREEN;
+					FinalArray[(i * 4) + 3] = tmp.BLUE;
+					++count;
+				}
+			}
+		}
+		return FinalArray;
+	}
+};
+template<size_t Size> int Combine<Size>::FinalArray[Size*Size * sizeof(__int20)] = { 0 };
 
-template<int index, typename _ListOfPixel> struct PixelAt {
-	typedef typename PixelAt<index - 1, _ListOfPixel>::result result;
-};
-template<typename _ListOfPixel> struct PixelAt<0, _ListOfPixel> {
-	typedef typename _ListOfPixel::head result;
-};
+/*  Here is the code for SmallBitmap  */
 
-template<typename px_first, typename px_others, typename... px, typename... px_2, typename... rest>
-struct ConcatPixels<ListOfPixels<px_first, px...>, ListOfPixels<px_others, px_2...>, rest...> {
-	typedef ListOfPixels<px_first, px_others, px..., px_2...> first_two;
-	typedef typename ConcatPixels<first_two, rest...>::result result;
+namespace SmallBitmap {
+	template<int Coord, typename _RGB> struct EncodedPixel {
+		static constexpr int coordinate = Coord;
+		static constexpr int Red_Value = _RGB::Red;
+		static constexpr int Green_Value = _RGB::Green;
+		static constexpr int Blue_Value = _RGB::Blue;
+	};
+	template<typename FirstEncodedPixel, typename... EncPx> struct ListOfPixels {
+		typedef FirstEncodedPixel head;
+		typedef ListOfPixels<EncPx...> rest;
+	};
+	template<typename FirstEncodedPixel> struct ListOfPixels<FirstEncodedPixel> {
+		typedef FirstEncodedPixel head;
+		typedef null rest;
+	};
+	template<typename... _ListOfPixel> struct ConcatanatePixels;
+	template<typename EncPx_first, typename EncPx_others, typename... EncPx, typename... EncPx_2, typename... rest>
+	struct ConcatanatePixels<ListOfPixels<EncPx_first, EncPx...>, ListOfPixels<EncPx_others, EncPx_2...>, rest...> {
+		typedef ListOfPixels<EncPx_first, EncPx_others, EncPx..., EncPx_2...> first_two;
+		typedef typename ConcatanatePixels<first_two, rest...>::result result;
+	};
+	template<typename EncPx_first, typename... EncPx> struct ConcatanatePixels<ListOfPixels<EncPx_first, EncPx...>> {
+		typedef ListOfPixels<EncPx_first, EncPx...> result;
+	};
+	template<typename EncPx_first> struct ConcatanatePixels<EncPx_first> {
+		typedef ListOfPixels<EncPx_first> result;
+	};
+	template<typename EncPx_first> struct ConcatanatePixels<EncPx_first, ListOfPixels<null>> {
+		typedef ListOfPixels<EncPx_first> result;
+	};
+	template<int at, typename list, typename EncPx> struct SubstitudeAt {
+		typedef ListOfPixels<typename list::head> HeadList;
+		typedef typename SubstitudeAt<at - 1, typename list::rest, EncPx>::result TailList;
+		typedef typename ConcatanatePixels<HeadList, TailList>::result result;
+	};
+	template<typename list, typename EncPx> struct SubstitudeAt<0, list, EncPx> {
+		typedef ListOfPixels<EncPx> temp_result;
+		typedef typename ConcatanatePixels<temp_result, typename list::rest>::result result;
+	};
+	template<int begin, int end> struct ZeroList {
+		typedef RGB<0, 0, 0> Black;
+		typedef EncodedPixel<begin, Black> InitialDense_Pixel;
+		typedef ListOfPixels<InitialDense_Pixel> HeadList;
+		typedef typename ZeroList<begin + 1, end>::result TailList;
+		typedef typename ConcatanatePixels<HeadList, TailList>::result result;
+	};
+	template<int end> struct ZeroList<end, end> {
+		typedef RGB<0, 0, 0> Black;
+		typedef EncodedPixel<end, Black> InitialDense_Pixel;
+		typedef ListOfPixels<InitialDense_Pixel> result;
+	};
+	template struct ZeroList<90,  500>;
+	template struct ZeroList<180, 500>;
+	template struct ZeroList<270, 500>;
+	template struct ZeroList<360, 500>;
+	template struct ZeroList<450, 500>;
+	template<int _Size, typename list, typename _List> struct ListMaker;
+	template<int _Size, typename list, typename First_EncPx, typename ...EncPxs> struct ListMaker<_Size, list, ListOfPixels<First_EncPx, EncPxs...>> {
+		static constexpr int temp_coord = First_EncPx::coordinate;
+		typedef typename SubstitudeAt<temp_coord, list, First_EncPx>::result substituted_list;
+		typedef typename ListMaker<_Size, substituted_list, ListOfPixels<EncPxs...>>::result result;
+	};
+	template<int _Size, typename list, typename First_EncPx> struct ListMaker<_Size, list, ListOfPixels<First_EncPx>> {
+		static constexpr int temp_coord = First_EncPx::coordinate;
+		typedef typename SubstitudeAt<temp_coord, list, First_EncPx>::result result;
+	};
+	template<int Square_Size, typename _ListOfPixels> struct SmallBitmap {
+		static constexpr int SquareSize = Square_Size;
+		typedef typename ZeroList<0, Square_Size>::result blackList;
+		typedef typename ListMaker<Square_Size, blackList, _ListOfPixels>::result result;
+	};
+	template<typename list> struct Print {
+		static void pr() {
+			std::cout << list::head::coordinate << "-> R: " <<
+				list::head::Red_Value << "-> G: " <<
+				list::head::Green_Value << "-> B: " <<
+				list::head::Blue_Value << std::endl;
+			Print<typename list::rest>::pr();
+		}
+	};
+	template<> struct Print<null> {
+		static void pr() {
+		}
+	};
+}
+	template<size_t __sz, typename...__px> struct Bitmap {
+		static constexpr int BitmapPixelSize = __Bitmap<__sz, __px...>::BitmapPixelSize;
+		static constexpr int BitmapLightPixelsListSize = __Bitmap<__sz, __px...>::BitmapLightPixelsListSize;
+		static constexpr __int20 LightBitmap[sizeof...(__px)] = __Bitmap<__sz, __px...>::LightBitmap;
+		static int* DecodedPixelList;
+	};
+template<size_t __sz, typename...__px> int* Bitmap<__sz, __px...>::DecodedPixelList = Combine<__Bitmap<__sz, __px...>::BitmapPixelSize>::perform(
+	__Bitmap<__sz, __px...>::LightBitmap, __Bitmap<__sz, __px...>::BitmapLightPixelsListSize
+);
+struct Print {
+	static void print(int* _bitmapList, size_t sz) {
+		for (size_t i = 0; i != sz*sz; ++i)
+		{
+			std::cout << "Pixel (" << i / sz << "," << i - (i / sz)*sz << "):" << std::endl;
+			std::cout << *(_bitmapList + (4 * i)) << std::endl;
+			std::cout << *(_bitmapList + (4 * i) + 1) << std::endl;
+			std::cout << *(_bitmapList + (4 * i) + 2) << std::endl;
+			std::cout << *(_bitmapList + (4 * i) + 3) << std::endl;
+			std::cout << "....." << std::endl;
+		}
+	}
 };
-template<typename px_first, typename... px>
-struct ConcatPixels<ListOfPixels<px_first, px...>> {
-	typedef ListOfPixels<px_first, px...> result;
-};
+int main()
+{
+	construct RGB<102, 11, 76>             BLENDEDCOLOR; 
+	construct RGB_From_Color<Color::WHITE> WHITE;
+	construct pixel<1, 2, BLENDEDCOLOR>    px1;
+	construct pixel<9, 6, WHITE>           px2;
+	construct pixel<9, 9, WHITE>           px3;
 
-template<typename _ListOfPixels> struct PixelnumbersInTheList {
-	static constexpr int number = SizeOfListOfPixels<_ListOfPixels>::__size;
-};
-template<int xbegin, int xend, int ybegin, int yend, typename _list> struct _YFor {
-	typedef RGB<0, 0, 0> Dark_RGB;
-	typedef ListOfPixels<_Null> FinalListOfPixels;
-	typedef ConcatPixels<FinalListOfPixels, ListOfPixels<typename Pixel<xbegin, ybegin, Dark_RGB>::result>> _FinalListOfPixels_;
-	typedef typename _YFor<xbegin, xend, ybegin + 1, yend, _FinalListOfPixels_>::result result;
-};
-template<int xbegin, int xend, int yend, typename _list> struct _YFor<xbegin, xend, yend, yend, _list> {
-	typedef RGB<0, 0, 0> Dark_RGB;
-	typedef ConcatPixels<_list, ListOfPixels<typename PixelAt<xend, _list>::result>> result;
-};
-template<int xbegin, int xend, int ybegin, int yend, typename _list> struct _XFor {
-	typedef RGB<0, 0, 0> Dark_RGB;
-	typedef ListOfPixels<_Null> FinalListOfPixels;
-	typedef ConcatPixels<FinalListOfPixels, ListOfPixels<typename Pixel<xbegin, ybegin, Dark_RGB>::result>> _FinalListOfPixels_;
-	typedef typename _YFor<xbegin, xend, ybegin, yend, _FinalListOfPixels_>::result result_star;
-	typedef typename _XFor<xbegin + 1, xend, ybegin, yend, result_star>::result result;
-};
-template<int xend, int ybegin, int yend, typename _list> struct _XFor<xend, xend, ybegin, yend, _list> {
-	typedef RGB<0, 0, 0> Dark_RGB;
-	typedef ConcatPixels<ListOfPixels<typename Pixel<xend, ybegin, Dark_RGB>::result>> _FinalListOfPixels_;
-	typedef typename _YFor<xend, xend, ybegin, yend, _FinalListOfPixels_>::result result;
-};
-
-template<int xLimit, int yLimit, typename _ListOfPixels> struct Fill {
-	typedef typename _XFor<0, xLimit, 0, yLimit, _ListOfPixels>::result result;
-};
-
-template<int Square_Size, typename _ListOfPixels> struct Bitmap {
-	static constexpr int squareSize = Square_Size;
-	typedef _ListOfPixels __ListOfPixels;
-	typedef typename Fill<128,128,_ListOfPixels>::result Array;
-};
-template<typename _Bitmap1, typename _Bitmap2, typename... _Bitmap> struct AddBitmaps {
-	static constexpr int squareSize = _Bitmap1::squareSize;
-	typedef typename _Bitmap1::__ListOfPixels firstBitmapListOfPixels;
-	typedef typename _Bitmap2::__ListOfPixels secondBitmapListOfPixels;
-	typedef ListOfPixels<
-				typename ConcatPixels<
-					firstBitmapListOfPixels,
-					secondBitmapListOfPixels
-				>::result
-								> resultedFirstTwoConcatanated;
-	typedef typename AddBitmaps<Bitmap<squareSize, resultedFirstTwoConcatanated>, _Bitmap...>::result result;
-};
-template<typename _Bitmap> struct AddBitmaps<_Bitmap, _Null> {
-	static constexpr int squareSize = _Bitmap::squareSize;
-	typedef typename Bitmap::__ListOfPixels firstBitmapListOfPixels;
-	typedef ListOfPixels<typename ConcatPixels<firstBitmapListOfPixels>::result> resultedLastConcatanated;
-	typedef typename Bitmap<squareSize, resultedLastConcatanated> result;
-};
-int main() {
-	construct RGB<1, 1, 1> White;
-	construct Bitmap<512, ListOfPixels<
-						Pixel<0, 1, White>,
-						Pixel<1, 1, White>,
-						Pixel<8, 6, White>
-									>
-										> bitmap;
-	construct Bitmap<512, ListOfPixels<
-						Pixel<0, 1, White>
-									>
-	                                                                	> bitmap2;
-	construct Bitmap<512, ListOfPixels<
-						Pixel<18, 1, White>,
-						Pixel<0, 21, White>
-									>
-										> bitmap3;
-	construct AddBitmaps<bitmap, bitmap2, bitmap3> sum;
-	construct Subtract<Color::WHITE, Subtract_Green> newColor;
+	constexpr int sz1 = 10;
+	typedef Bitmap<sz1, px1, px2, px3> bitmap;
+	Print::print (
+		bitmap::DecodedPixelList,
+		bitmap::BitmapPixelSize
+	);
+	construct SmallBitmap::SmallBitmap<500, SmallBitmap::ListOfPixels<
+																		SmallBitmap::EncodedPixel<1,   WHITE>,
+																		SmallBitmap::EncodedPixel<12,  WHITE>,
+																		SmallBitmap::EncodedPixel<15,  WHITE>,
+																		SmallBitmap::EncodedPixel<45,  WHITE>,
+																		SmallBitmap::EncodedPixel<96,  WHITE>,
+																		SmallBitmap::EncodedPixel<127, WHITE>
+																												>
+																													> bitmap_2;
+	SmallBitmap::Print<bitmap_2::result>::pr();
 }
